0a4705ddab4b6f02ff53a6087883bf50c455d291
MikuroXina <ryosukadnak@gmail.com>
2023-11-01 07:48:30 +0900

    Add accounts package base

---
+++ main.ts
@@ -0,0 +1,8 @@
+import { Hono } from "https://deno.land/x/hono@v3.8.2/mod.ts";
+import { accounts } from "./pkg/accounts/mod.ts";
+
+const app = new Hono();
+
+app.route("/accounts", accounts);
+
+Deno.serve({ port: 3000 }, app.fetch);
