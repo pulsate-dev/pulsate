e74782b861932d62f0d378d80ed8dc850a0b3578
Tatsuto YAMAMOTO <me@laminne33569.net>
2023-11-13 22:15:41 +0900

    fix: repository method naming

--- pkg/accounts/model/repository.ts
+++ pkg/accounts/model/repository.ts
@@ -3,19 +3,19 @@ import { Account, AccountID } from './account.ts';
 import { ID } from '../../id/type.ts';
 
 export interface AccountRepository {
-  Create(account: Account): Promise<Result.Result<Error, void>>;
-  FindByName(name: string): Promise<Option.Option<Account>>;
-  FindByMail(mail: string): Promise<Option.Option<Account>>;
+  create(account: Account): Promise<Result.Result<Error, void>>;
+  findByName(name: string): Promise<Option.Option<Account>>;
+  findByMail(mail: string): Promise<Option.Option<Account>>;
 }
 
 export interface AccountVerifyTokenRepository {
-  Create(
+  create(
     accountID: ID<AccountID>,
     token: string,
     expire: Date,
   ): Promise<Result.Result<Error, void>>;
   // TODO(laminne): Consider create a type for token/expire
-  FindByID(
+  findByID(
     id: ID<AccountID>,
   ): Promise<Option.Option<{ token: string; expire: Date }>>;
 }
