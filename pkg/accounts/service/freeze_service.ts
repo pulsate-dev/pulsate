7e5b18b261884d45dfaa992189967c2a9bb98ec8
m1sk9 <me@m1sk9.dev>
2023-11-15 22:59:42 +0900

    feat: add `undoFreeze` method to `FreezeService`

--- pkg/accounts/service/freeze_service.ts
+++ pkg/accounts/service/freeze_service.ts
@@ -21,4 +21,20 @@ export class FreezeService {
       return Result.err(e);
     }
   }
+
+  async undoFreeze(
+    accountName: string,
+  ): Promise<Result.Result<Error, boolean>> {
+    const account = await this.accountRepository.findByName(accountName);
+    if (Option.isNone(account)) {
+      return Result.err(new Error('account not found'));
+    }
+
+    try {
+      account[1].setUnfreeze();
+      return Result.ok(true);
+    } catch (e) {
+      return Result.err(e);
+    }
+  }
 }
