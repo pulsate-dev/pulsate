91b0d6c67b5efe8ca3a49fbd0d8bbe7b579f7959
aqyuki <yksk171118@gmail.com>
2023-11-17 10:12:42 +0900

    chore: remove compare methods

--- pkg/accounts/service/etag_generate_service.ts
+++ pkg/accounts/service/etag_generate_service.ts
@@ -1,27 +1,7 @@
-import { Option, Result } from 'mini-fn';
 import { encodeHex } from 'std/encoding/hex';
-import { AccountRepository } from '../model/repository.ts';
 import { Account } from '../model/account.ts';
 
 export class EtagGenerateService {
-  private repository: AccountRepository;
-
-  constructor(repository: AccountRepository) {
-    this.repository = repository;
-  }
-
-  async compare(
-    name: string,
-    etag: string,
-  ): Promise<Result.Result<Error, boolean>> {
-    const account = await this.repository.findByName(name);
-    if (Option.isNone(account)) {
-      return Result.err(new Error('account not found'));
-    }
-    const generatedEtag = await this.generate(account[1]);
-    return Result.ok(etag === generatedEtag);
-  }
-
   async generate(account: Account): Promise<string> {
     const src = `${account.getNickname}:${account.getMail}`;
     const digest = await crypto.subtle.digest(
