90fbf576dffbfd9098eb0e1b7e267eaf1df67295
aqyuki <yksk171118@gmail.com>
2023-11-17 11:01:27 +0900

    feat: add compare methods

--- pkg/accounts/service/etag_verify_generate_service.ts
+++ pkg/accounts/service/etag_verify_generate_service.ts
@@ -2,6 +2,11 @@ import { encodeHex } from 'std/encoding/hex';
 import { Account } from '../model/account.ts';
 
 export class EtagVerifyService {
+  async compare(account: Account, etag: string): Promise<boolean> {
+    const generated = await this.generate(account);
+    return generated === etag;
+  }
+
   async generate(account: Account): Promise<string> {
     const src = `${account.getNickname}:${account.getMail}`;
     const digest = await crypto.subtle.digest(
