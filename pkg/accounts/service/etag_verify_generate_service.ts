93c1783ef38a29b0b6571ef98b76410805c0c0e8
aqyuki <yksk171118@gmail.com>
2023-11-17 11:15:33 +0900

    chore: add TSDoc

--- pkg/accounts/service/etag_verify_generate_service.ts
+++ pkg/accounts/service/etag_verify_generate_service.ts
@@ -2,11 +2,22 @@ import { encodeHex } from 'std/encoding/hex';
 import { Account } from '../model/account.ts';
 
 export class EtagVerifyService {
+  /**
+   * Compare Etag with Etag in given account.
+   * @param account
+   * @param etag
+   * @returns if success: true, if failure: false
+   */
   async compare(account: Account, etag: string): Promise<boolean> {
     const generated = await this.generate(account);
     return generated === etag;
   }
 
+  /**
+   * Generate Etag from given account.
+   * @param account
+   * @returns Etag
+   */
   async generate(account: Account): Promise<string> {
     const src = `${account.getNickname}:${account.getMail}`;
     const digest = await crypto.subtle.digest(
