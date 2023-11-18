7613f369763e6092621cd76047e5abdca794399c
aqyuki <yksk171118@gmail.com>
2023-11-18 18:48:39 +0900

    chore: change test style

--- pkg/accounts/service/etag_generate_verift_service.test.ts
+++ pkg/accounts/service/etag_generate_verift_service.test.ts
@@ -1,56 +1,88 @@
 import { EtagVerifyService } from './etag_verify_generate_service.ts';
-import { ID } from '../../id/type.ts';
-import { Account, AccountID, CreateAccountArgs } from '../model/account.ts';
 import { assertEquals } from 'std/assert';
-import { InMemoryAccountRepository } from '../adaptor/repository/dummy.ts';
+import {
+  InMemoryAccountRepository,
+  InMemoryAccountVerifyTokenRepository,
+} from '../adaptor/repository/dummy.ts';
 import { Result } from 'mini-fn';
+import { RegisterAccountService } from './register_service.ts';
+import { Clock, SnowflakeIDGenerator } from '../../id/mod.ts';
+import { ScryptPasswordEncoder } from '../../password/mod.ts';
+import { DummySendNotificationService } from './send_notification_service.ts';
+import { TokenVerifyService } from './token_verify_service.ts';
+import { AccountRole } from '../model/account.ts';
 
+class DummyClock implements Clock {
+  Now(): bigint {
+    return BigInt(new Date('2023/9/10 00:00:00 UTC').getTime());
+  }
+}
 const repository = new InMemoryAccountRepository();
-const service: EtagVerifyService = new EtagVerifyService();
+const verifyRepository = new InMemoryAccountVerifyTokenRepository();
+const registerService = new RegisterAccountService({
+  repository: repository,
+  idGenerator: new SnowflakeIDGenerator(1, new DummyClock()),
+  passwordEncoder: new ScryptPasswordEncoder(),
+  sendNotification: new DummySendNotificationService(),
+  verifyTokenService: new TokenVerifyService(verifyRepository),
+});
+const etagVerifyService: EtagVerifyService = new EtagVerifyService();
 
-const accountArgs: CreateAccountArgs = {
-  id: '1' as ID<AccountID>,
-  bio: 'this is john doe\'s account!',
-  createdAt: new Date('2023-09-10T00:00:00.000Z'),
-  mail: 'test@mail.example.com',
+const exampleInput = {
+  name: 'john_doe@example.com',
+  mail: 'johndoe@example.com',
   nickname: 'John Doe',
-  passphraseHash: 'leknflkwnrigohidvlk',
-  role: 'admin',
-  status: 'active',
-  frozen: 'frozen',
-  silenced: 'silenced',
-  name: 'johndoe@social.example.com',
-  updatedAt: new Date('2023-09-10T09:00:00.000Z'),
-  deletedAt: new Date('2023-09-10T10:00:00.000Z'),
+  passphrase: 'password',
+  bio: 'Hello, World!',
+  role: 'normal' as AccountRole,
 };
 
 Deno.test('success to verify etag', async () => {
-  const account = Account.new(accountArgs);
-  repository.create(account);
-  const res = await repository.create(account);
-  assertEquals(Result.isErr(res), false);
+  const res = await registerService.handle(
+    exampleInput.name,
+    exampleInput.mail,
+    exampleInput.nickname,
+    exampleInput.passphrase,
+    exampleInput.bio,
+    exampleInput.role,
+  );
+  const account = Result.unwrap(res);
 
-  const etag = await service.generate(account);
-  const result = await service.compare(account, etag);
+  const etag = await etagVerifyService.generate(account);
+  const result = await etagVerifyService.compare(account, etag);
   assertEquals(result, true);
   repository.reset();
 });
 
 Deno.test('failed to verify etag', async () => {
-  const account = Account.new(accountArgs);
-  repository.create(account);
+  const res = await registerService.handle(
+    exampleInput.name,
+    exampleInput.mail,
+    exampleInput.nickname,
+    exampleInput.passphrase,
+    exampleInput.bio,
+    exampleInput.role,
+  );
+  const account = Result.unwrap(res);
 
-  const etag = await service.generate(account) + '_invalid';
-  const result = await service.compare(account, etag);
+  const etag = await etagVerifyService.generate(account) + '_invalid';
+  const result = await etagVerifyService.compare(account, etag);
   assertEquals(result, false);
   repository.reset();
 });
 
 Deno.test('should return string which is 64 characters long', async () => {
-  const account = Account.new(accountArgs);
-  repository.create(account);
+  const res = await registerService.handle(
+    exampleInput.name,
+    exampleInput.mail,
+    exampleInput.nickname,
+    exampleInput.passphrase,
+    exampleInput.bio,
+    exampleInput.role,
+  );
+  const account = Result.unwrap(res);
 
-  const etag = await service.generate(account);
+  const etag = await etagVerifyService.generate(account);
   assertEquals(etag.length, 64);
   repository.reset();
 });
