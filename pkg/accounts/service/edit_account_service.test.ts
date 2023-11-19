6bd3fda2404f83f84227454651297ec9241b1616
K.Yosuke <117277631+aqyuki@users.noreply.github.com>
2023-11-19 16:16:08 +0900

    feat: implements `EditAccountService` (#96)

---
+++ pkg/accounts/service/edit_account_service.test.ts
@@ -0,0 +1,197 @@
+import {
+  InMemoryAccountRepository,
+  InMemoryAccountVerifyTokenRepository,
+} from '../adaptor/repository/dummy.ts';
+import { Clock, SnowflakeIDGenerator } from '../../id/mod.ts';
+import { ScryptPasswordEncoder } from '../../password/mod.ts';
+import { RegisterAccountService } from './register_service.ts';
+import { DummySendNotificationService } from './send_notification_service.ts';
+import { TokenVerifyService } from './token_verify_service.ts';
+import { EtagVerifyService } from './etag_verify_generate_service.ts';
+import { EditAccountService } from './edit_account_service.ts';
+
+import { AccountRole } from '../model/account.ts';
+import { Result } from 'mini-fn';
+import { assertEquals } from 'std/assert';
+
+const repository = new InMemoryAccountRepository();
+const verifyRepository = new InMemoryAccountVerifyTokenRepository();
+class DummyClock implements Clock {
+  Now(): bigint {
+    return BigInt(new Date('2023/9/10 00:00:00 UTC').getTime());
+  }
+}
+const registerService: RegisterAccountService = new RegisterAccountService({
+  repository,
+  idGenerator: new SnowflakeIDGenerator(1, new DummyClock()),
+  passwordEncoder: new ScryptPasswordEncoder(),
+  sendNotification: new DummySendNotificationService(),
+  verifyTokenService: new TokenVerifyService(verifyRepository),
+});
+const etagVerifyService = new EtagVerifyService();
+const editAccountService = new EditAccountService(
+  repository,
+  etagVerifyService,
+  new ScryptPasswordEncoder(),
+);
+
+const exampleInput = {
+  name: 'john_doe@example.com',
+  mail: 'johndoe@example.com',
+  nickname: 'John Doe',
+  passphrase: 'password',
+  bio: 'Hello, World!',
+  role: 'normal' as AccountRole,
+};
+
+Deno.test('should be success to update nickname', async () => {
+  const res = await registerService.handle(
+    exampleInput.name,
+    exampleInput.mail,
+    exampleInput.nickname,
+    exampleInput.passphrase,
+    exampleInput.bio,
+    exampleInput.role,
+  );
+  const account = Result.unwrap(res);
+  const etag = await etagVerifyService.generate(account);
+  const updateRes = await editAccountService.editNickname(
+    etag,
+    exampleInput.name,
+    'new nickname',
+  );
+  assertEquals(Result.isErr(updateRes), false);
+  assertEquals(updateRes[1], true);
+  assertEquals(account.getNickname, 'new nickname');
+  repository.reset();
+});
+
+Deno.test('should be fail to update nickname when etag not match', async () => {
+  await registerService.handle(
+    exampleInput.name,
+    exampleInput.mail,
+    exampleInput.nickname,
+    exampleInput.passphrase,
+    exampleInput.bio,
+    exampleInput.role,
+  );
+
+  const res = await editAccountService.editNickname(
+    'invalid_etag',
+    exampleInput.name,
+    'new nickname',
+  );
+  assertEquals(Result.isErr(res), true);
+  repository.reset();
+});
+
+Deno.test('should be fail to update nickname when account not found', async () => {
+  const res = await editAccountService.editNickname(
+    'invalid etag',
+    'foo',
+    'new nickname',
+  );
+  assertEquals(Result.isErr(res), true);
+  repository.reset();
+});
+
+Deno.test('should be success to update passphrase', async () => {
+  const res = await registerService.handle(
+    exampleInput.name,
+    exampleInput.mail,
+    exampleInput.nickname,
+    exampleInput.passphrase,
+    exampleInput.bio,
+    exampleInput.role,
+  );
+  const account = Result.unwrap(res);
+  const etag = await etagVerifyService.generate(account);
+
+  const updateRes = await editAccountService.editPassphrase(
+    etag,
+    exampleInput.name,
+    'new password',
+  );
+  assertEquals(Result.isErr(updateRes), false);
+  assertEquals(updateRes[1], true);
+  repository.reset();
+});
+
+Deno.test('should be fail to update passphrase when etag not match', async () => {
+  await registerService.handle(
+    exampleInput.name,
+    exampleInput.mail,
+    exampleInput.nickname,
+    exampleInput.passphrase,
+    exampleInput.bio,
+    exampleInput.role,
+  );
+
+  const res = await editAccountService.editPassphrase(
+    'invalid_etag',
+    exampleInput.name,
+    'new password',
+  );
+  assertEquals(Result.isErr(res), true);
+  repository.reset();
+});
+
+Deno.test('should be fail to update passphrase when account not found', async () => {
+  const res = await editAccountService.editPassphrase(
+    'invalid etag',
+    'foo',
+    'new password',
+  );
+  assertEquals(Result.isErr(res), true);
+  repository.reset();
+});
+
+Deno.test('should be success to update email', async () => {
+  const res = await registerService.handle(
+    exampleInput.name,
+    exampleInput.mail,
+    exampleInput.nickname,
+    exampleInput.passphrase,
+    exampleInput.bio,
+    exampleInput.role,
+  );
+  const account = Result.unwrap(res);
+  const etag = await etagVerifyService.generate(account);
+
+  const updateRes = await editAccountService.editEmail(
+    etag,
+    exampleInput.name,
+    'pulsate@example.com',
+  );
+  assertEquals(Result.isErr(updateRes), false);
+  assertEquals(updateRes[1], true);
+  repository.reset();
+});
+
+Deno.test('should be fail to update email when etag not match', async () => {
+  const res = await registerService.handle(
+    exampleInput.name,
+    exampleInput.mail,
+    exampleInput.nickname,
+    exampleInput.passphrase,
+    exampleInput.bio,
+    exampleInput.role,
+  );
+  Result.unwrap(res);
+
+  const updateRes = await editAccountService.editEmail(
+    'invalid_etag',
+    exampleInput.name,
+    'pulsate@example.com',
+  );
+  assertEquals(Result.isErr(updateRes), true);
+});
+
+Deno.test('should be fail to update email when account not found', async () => {
+  const updateRes = await editAccountService.editEmail(
+    'invalid etag',
+    'foo',
+    'pulsate@pulsate.mail',
+  );
+  assertEquals(Result.isErr(updateRes), true);
+});
---
+++ pkg/accounts/service/edit_account_service.ts
@@ -0,0 +1,97 @@
+import { Option, Result } from 'mini-fn';
+import { EtagVerifyService } from './etag_verify_generate_service.ts';
+import { AccountRepository } from '../model/repository.ts';
+import { PasswordEncoder } from '../../password/mod.ts';
+
+export class EditAccountService {
+  private accountRepository: AccountRepository;
+  private etagVerifyService: EtagVerifyService;
+  private passwordEncoder: PasswordEncoder;
+
+  constructor(
+    accountRepository: AccountRepository,
+    etagVerifyService: EtagVerifyService,
+    passwordEncoder: PasswordEncoder,
+  ) {
+    this.accountRepository = accountRepository;
+    this.etagVerifyService = etagVerifyService;
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  async editNickname(
+    etag: string,
+    name: string,
+    nickname: string,
+  ): Promise<Result.Result<Error, boolean>> {
+    const res = await this.accountRepository.findByName(name);
+    if (Option.isNone(res)) {
+      return Result.err(new Error('account not found'));
+    }
+    const account = Option.unwrap(res);
+
+    const match = await this.etagVerifyService.Verify(account, etag);
+    if (!match) {
+      // TODO: add a new error type for etag not match
+      return Result.err(new Error('etag not match'));
+    }
+
+    try {
+      account.setNickName(nickname);
+      return Result.ok(true);
+    } catch (e) {
+      return Result.err(e);
+    }
+  }
+
+  async editPassphrase(
+    etag: string,
+    name: string,
+    newPassphrase: string,
+  ): Promise<Result.Result<Error, boolean>> {
+    const res = await this.accountRepository.findByName(name);
+    if (Option.isNone(res)) {
+      return Result.err(new Error('account not found'));
+    }
+    const account = Option.unwrap(res);
+
+    const match = await this.etagVerifyService.Verify(account, etag);
+    if (!match) {
+      return Result.err(new Error('etag not match'));
+    }
+
+    try {
+      account.setPassphraseHash(
+        this.passwordEncoder.EncodePasword(newPassphrase),
+      );
+      return Result.ok(true);
+    } catch (e) {
+      return Result.err(e);
+    }
+  }
+
+  async editEmail(
+    etag: string,
+    name: string,
+    newEmail: string,
+  ): Promise<Result.Result<Error, boolean>> {
+    const res = await this.accountRepository.findByName(name);
+    if (Option.isNone(res)) {
+      return Result.err(new Error('account not found'));
+    }
+    const account = Option.unwrap(res);
+
+    const match = await this.etagVerifyService.Verify(account, etag);
+    if (!match) {
+      return Result.err(new Error('etag not match'));
+    }
+
+    // TODO: add a process to check the email is active
+
+    try {
+      account.setMail(newEmail);
+      return Result.ok(true);
+    } catch (e) {
+      return Result.err(e);
+    }
+  }
+}
