1bf5ffb8075235b64d40ba9285f2ae8855f831e6
Tatsuto YAMAMOTO <me@laminne33569.net>
2023-12-06 23:57:13 +0900

    feat(docs): define api route/schema

--- main.ts
+++ main.ts
@@ -1,50 +1,59 @@
 import { Hono } from 'hono';
 import { accounts } from './pkg/accounts/mod.ts';
-import {apiReference} from "scalar/hono-api-reference"
+import { apiReference } from 'scalar/hono-api-reference';
 
 const app = new Hono();
 
 app.route('/', accounts);
-app.get("/notes", (c) => {
-  return c.text("Hello, World!");
-})
+app.get('/notes', (c) => {
+  return c.text('Hello, World!');
+});
 
-app.get("/doc.json", async (c) => {
-  const modulePath: Array<string> = ["accounts"];
-  const basePath = "http://localhost:3000/";
+app.get('/doc.json', async (c) => {
+  const modulePath: string[] = ['accounts'];
+  const basePath = 'http://localhost:3000/';
   const openAPIBase = {
-    "openapi": "3.0.0",
-    "info": {
-      "title": "Pulsate API Document",
-      "version": "0.1.0",
+    'openapi': '3.0.0',
+    'info': {
+      'description': '',
+      'title': 'Pulsate API Document',
+      'version': '0.1.0',
     },
-    "components": {
-      "schemas": {},
-      "parameters": {},
+    'components': {
+      'schemas': {},
+      'parameters': {},
     },
-    "paths": {}
-  }
+    'paths': {},
+  };
 
   const res = modulePath.map(async (path) => {
-    return (await fetch(basePath + path + "/doc.json")).json();
+    return (await fetch(basePath + path + '/doc.json')).json();
   });
 
   for (const v in res) {
     const data = await res[v];
-    openAPIBase.components.schemas = Object.assign(openAPIBase.components.schemas, data.components.schemas);
-    openAPIBase.components.parameters = Object.assign(openAPIBase.components.parameters, data.components.parameters);
+    openAPIBase.components.schemas = Object.assign(
+      openAPIBase.components.schemas,
+      data.components.schemas,
+    );
+    openAPIBase.components.parameters = Object.assign(
+      openAPIBase.components.parameters,
+      data.components.parameters,
+    );
     openAPIBase.paths = Object.assign(openAPIBase.paths, data.paths);
   }
 
-  return c.json(openAPIBase)
+  return c.json(openAPIBase);
 });
 
-
-app.get("/reference", apiReference({
-  pageTitle: "Pulsate API",
-  spec: {
-    url: "/doc.json",
-  }
-}));
+app.get(
+  '/reference',
+  apiReference({
+    pageTitle: 'Pulsate API',
+    spec: {
+      url: '/doc.json',
+    },
+  }),
+);
 
 Deno.serve({ port: 3000 }, app.fetch);
--- pkg/accounts/router.ts
+++ pkg/accounts/router.ts
@@ -1,32 +1,28 @@
 import { createRoute, z } from 'hono/zod-openapi';
-
-const CreateAccountRequestSchema = z.object({
-  // ToDo: 文字種制約/先頭,末尾制約の実装
-  name: z.string().min(3).max(64).openapi({
-    example: 'example_man',
-  }),
-  email: z.string().email().openapi({ example: 'foo@example.com' }),
-  passphrase: z.string().min(8).max(512).openapi({
-    example: 'じゃすた・いぐざんぽぅ',
-  }),
-  captcha_token: z.string(),
-}).openapi('CreateAccountRequest');
-
-const CreateAccountResponseSchema = z.object({
-  id: z.string().openapi({
-    example: '38477395',
-  }),
-  name: z.string().openapi({ example: 'example_man@example.com' }),
-  email: z.string().email().openapi({ example: 'foo@example.com' }),
-}).openapi('CreateAccountResponse');
+import {
+  CommonErrorResponseSchema,
+  CreateAccountRequestSchema,
+  CreateAccountResponseSchema,
+  FollowAccountResponseSchema,
+  GetAccountResponseSchema,
+  LoginRequestSchema,
+  RefreshRequestSchema,
+  ResendVerificationEmailRequestSchema,
+  UpdateAccountRequestSchema,
+  UpdateAccountResponseSchema,
+  VerifyEmailRequestSchema,
+} from './adaptor/validator/schema.ts';
 
 export const CreateAccountRoute = createRoute({
   method: 'post',
+  tags: ['accounts'],
   path: '/accounts',
   request: {
     body: {
       content: {
-        'application/json': { schema: CreateAccountRequestSchema },
+        'application/json': {
+          schema: CreateAccountRequestSchema,
+        },
       },
     },
   },
@@ -34,7 +30,552 @@ export const CreateAccountRoute = createRoute({
     200: {
       description: 'OK',
       content: {
-        'application/json': { schema: CreateAccountResponseSchema },
+        'application/json': {
+          schema: CreateAccountResponseSchema,
+        },
+      },
+    },
+    400: {
+      description: 'Bad Request',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    409: {
+      description: 'Conflict',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+  },
+});
+
+export const UpdateAccountRoute = createRoute({
+  method: 'patch',
+  tags: ['accounts'],
+  path: '/accounts/:name',
+  request: {
+    // ToDo: define ETag in header
+    params: z.object({
+      name: z.string().min(3).max(64).openapi({
+        example: 'example_man',
+        description:
+          'Characters must be [A-Za-z0-9-\.] The first and last characters must be [A-Za-z0-9-\.]',
+      }),
+    }),
+    body: {
+      content: {
+        'application/json': {
+          schema: UpdateAccountRequestSchema,
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'OK',
+      content: {
+        'application/json': {
+          schema: UpdateAccountResponseSchema,
+        },
+      },
+    },
+    202: {
+      description: 'When email updated',
+      content: {
+        'application/json': {
+          schema: UpdateAccountResponseSchema,
+        },
+      },
+    },
+    400: {
+      description: 'Bad Request',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    404: {
+      description: 'Not Found',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    412: {
+      description: 'Precondition Failed',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+  },
+});
+
+export const FreezeAccountRoute = createRoute({
+  method: 'put',
+  tags: ['accounts'],
+  path: '/accounts/:name/freeze',
+  request: {
+    params: z.object({
+      name: z.string().min(3).max(64).openapi({
+        example: 'example_man',
+        description:
+          'Characters must be [A-Za-z0-9-\.] The first and last characters must be [A-Za-z0-9-\.]',
+      }),
+    }),
+  },
+  responses: {
+    204: {
+      description: 'No Content',
+    },
+    400: {
+      description: 'Bad Request',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    403: {
+      description: 'Forbidden',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    404: {
+      description: 'Not Found',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+  },
+});
+
+export const UnFreezeAccountRoute = createRoute({
+  method: 'delete',
+  tags: ['accounts'],
+  path: '/accounts/:name/freeze',
+  request: {
+    params: z.object({
+      name: z.string().min(3).max(64).openapi({
+        example: 'example_man',
+        description:
+          'Characters must be [A-Za-z0-9-\.] The first and last characters must be [A-Za-z0-9-\.]',
+      }),
+    }),
+  },
+  responses: {
+    204: {
+      description: 'No Content',
+    },
+    400: {
+      description: 'Bad Request',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    403: {
+      description: 'Forbidden',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    404: {
+      description: 'Not Found',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+  },
+});
+
+export const ResendVerificationEmailRoute = createRoute({
+  method: 'post',
+  tags: ['accounts'],
+  path: '/accounts/:name/resend_verify_email',
+  request: {
+    params: z.object({
+      name: z.string().min(3).max(64).openapi({
+        example: 'example_man',
+        description:
+          'Characters must be [A-Za-z0-9-\.] The first and last characters must be [A-Za-z0-9-\.]',
+      }),
+    }),
+    body: {
+      content: {
+        'application/json': {
+          schema: ResendVerificationEmailRequestSchema,
+        },
+      },
+    },
+  },
+  responses: {
+    204: {
+      description: 'No Content',
+    },
+    400: {
+      description: 'Bad Request',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    403: {
+      description: 'Forbidden',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    404: {
+      description: 'Not Found',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+  },
+});
+
+export const VerifyEmailRoute = createRoute({
+  method: 'post',
+  tags: ['accounts'],
+  path: '/accounts/:name/verify_email',
+  request: {
+    params: z.object({
+      name: z.string().min(3).max(64).openapi({
+        example: 'example_man',
+        description:
+          'Characters must be [A-Za-z0-9-\.] The first and last characters must be [A-Za-z0-9-\.]',
+      }),
+    }),
+    body: {
+      content: {
+        'application/json': {
+          schema: VerifyEmailRequestSchema,
+        },
+      },
+    },
+  },
+  responses: {
+    204: {
+      description: 'No Content',
+    },
+    400: {
+      description: 'Bad Request',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    404: {
+      description: 'Not Found',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+  },
+});
+
+export const LoginRoute = createRoute({
+  method: 'post',
+  tags: ['accounts'],
+  path: '/login',
+  request: {
+    body: {
+      content: {
+        'application/json': {
+          schema: LoginRequestSchema,
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'OK',
+      content: {
+        'application/json': {
+          schema: UpdateAccountResponseSchema,
+        },
+      },
+    },
+    400: {
+      description: 'Bad Request',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    403: {
+      description: 'Forbidden',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+  },
+});
+
+export const RefreshRoute = createRoute({
+  method: 'post',
+  tags: ['accounts'],
+  path: '/refresh',
+  request: {
+    body: {
+      content: {
+        'application/json': {
+          schema: RefreshRequestSchema,
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: 'OK',
+      content: {
+        'application/json': {
+          schema: UpdateAccountResponseSchema,
+        },
+      },
+    },
+    400: {
+      description: 'Bad Request',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+  },
+});
+
+export const GetAccountRoute = createRoute({
+  method: 'get',
+  tags: ['accounts'],
+  path: '/accounts/:name',
+  request: {
+    params: z.object({
+      name: z.string().min(3).max(64).openapi({
+        example: 'example_man',
+        description:
+          'Characters must be [A-Za-z0-9-\.] The first and last characters must be [A-Za-z0-9-\.]',
+      }),
+    }),
+  },
+  responses: {
+    200: {
+      description: 'OK',
+      content: {
+        'application/json': {
+          schema: GetAccountResponseSchema,
+        },
+      },
+    },
+  },
+});
+
+export const SilenceAccountRoute = createRoute({
+  method: 'put',
+  tags: ['accounts'],
+  path: '/accounts/:name/silence',
+  request: {
+    params: z.object({
+      name: z.string().min(3).max(64).openapi({
+        example: 'example_man',
+        description:
+          'Characters must be [A-Za-z0-9-\.] The first and last characters must be [A-Za-z0-9-\.]',
+      }),
+    }),
+    body: {
+      content: {
+        'application/json': {
+          // empty body
+          schema: {},
+        },
+      },
+    },
+  },
+  responses: {
+    204: {
+      description: 'No Content',
+    },
+    403: {
+      description: 'Forbidden',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    404: {
+      description: 'Not Found',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+  },
+});
+
+export const UnSilenceAccountRoute = createRoute({
+  method: 'delete',
+  tags: ['accounts'],
+  path: '/accounts/:name/silence',
+  request: {
+    params: z.object({
+      name: z.string().min(3).max(64).openapi({
+        example: 'example_man',
+        description:
+          'Characters must be [A-Za-z0-9-\.] The first and last characters must be [A-Za-z0-9-\.]',
+      }),
+    }),
+    body: {
+      content: {
+        'application/json': {
+          // empty body
+          schema: {},
+        },
+      },
+    },
+  },
+  responses: {
+    204: {
+      description: 'No Content',
+    },
+    403: {
+      description: 'Forbidden',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    404: {
+      description: 'Not Found',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+  },
+});
+
+export const FollowAccountRoute = createRoute({
+  method: 'post',
+  tags: ['accounts'],
+  path: '/accounts/:name/follow',
+  request: {
+    params: z.object({
+      name: z.string().min(3).max(64).openapi({
+        example: 'example_man',
+        description:
+          'Characters must be [A-Za-z0-9-\.] The first and last characters must be [A-Za-z0-9-\.]',
+      }),
+    }),
+    body: {
+      content: {
+        'application/json': {
+          // empty body
+          schema: {},
+        },
+      },
+    },
+  },
+  responses: {
+    201: {
+      description: 'Accepted',
+      content: {
+        'application/json': {
+          schema: FollowAccountResponseSchema,
+        },
+      },
+    },
+    403: {
+      description: 'Forbidden',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    404: {
+      description: 'Not Found',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+  },
+});
+
+export const UnFollowAccountRoute = createRoute({
+  method: 'delete',
+  tags: ['accounts'],
+  path: '/accounts/:name/follow',
+  request: {
+    params: z.object({
+      name: z.string().min(3).max(64).openapi({
+        example: 'example_man',
+        description:
+          'Characters must be [A-Za-z0-9-\.] The first and last characters must be [A-Za-z0-9-\.]',
+      }),
+    }),
+    body: {
+      content: {
+        'application/json': {
+          // empty body
+          schema: {},
+        },
+      },
+    },
+  },
+  responses: {
+    204: {
+      description: 'No Content',
+    },
+    400: {
+      description: 'Forbidden',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
+      },
+    },
+    404: {
+      description: 'Not Found',
+      content: {
+        'application/json': {
+          schema: CommonErrorResponseSchema,
+        },
       },
     },
   },
