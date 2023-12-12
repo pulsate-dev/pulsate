3c108ac3c366f862e7baf6f2688d76482ab89166
Tatsuto YAMAMOTO <me@laminne33569.net>
2023-12-12 19:45:28 +0900

    fix: remove unused microservice files (#104)

--- staging/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "service_account_github_actions_email" {
-  description = "github account for github actions"
-  value       = google_service_account.github_actions.email
-}
-
-output "google_iam_workload_identity_pool_provider_github_name" {
-  description = "Workload Identity Pood Provider ID"
-  value       = google_iam_workload_identity_pool_provider.github.name
-}
