2a69ec33e16d6bda4b0a2bb10c96e69c7deb0fb2
Tatsuto YAMAMOTO <me@laminne33569.net>
2023-12-15 18:58:34 +0900

    feat: impl for ResendTokenService

--- pkg/accounts/service/resendToken.ts
+++ pkg/accounts/service/resendToken.ts
@@ -1,15 +1,40 @@
-import { AccountRepository, AccountVerifyTokenRepository } from '../model/repository.ts';
+import { Option, Result } from 'mini-fn';
+import { AccountRepository } from '../model/repository.ts';
+import { SendNotificationService } from './sendNotification.ts';
+import { TokenVerifyService } from './tokenVerify.ts';
 
 export class ResendVerifyTokenService {
-private readonly accountRepository: AccountRepository;
-private readonly verifyTokenRepository: AccountVerifyTokenRepository;
+  private readonly accountRepository: AccountRepository;
+  private readonly tokenVerifyService: TokenVerifyService;
+  private readonly sendNotificationService: SendNotificationService;
 
-  constructor(accountRepository: AccountRepository, verifyTokenRepository: AccountVerifyTokenRepository) {
+  constructor(
+    accountRepository: AccountRepository,
+    tokenVerifyService: TokenVerifyService,
+    sendNotificationService: SendNotificationService,
+  ) {
     this.accountRepository = accountRepository;
-    this.verifyTokenRepository = verifyTokenRepository;
+    this.tokenVerifyService = tokenVerifyService;
+    this.sendNotificationService = sendNotificationService;
   }
 
-  async handle(name: string) {
-    throw new Error('Not implemented');
+  async handle(name: string): Promise<Option.Option<Error>> {
+    const account = await this.accountRepository.findByName(name);
+    if (Option.isNone(account)) {
+      return Option.some(new Error('AccountNotFoundError'));
+    }
+
+    if (account[1].getStatus !== 'notActivated') {
+      return Option.some(new Error('AccountAlreadyVerifiedError'));
+    }
+
+    const token = await this.tokenVerifyService.generate(account[1].getID);
+    if (Result.isErr(token)) {
+      return Option.some(token[1]);
+    }
+
+    this.sendNotificationService.Send(account[1].getMail, token[1]);
+
+    return Option.none();
   }
 }
