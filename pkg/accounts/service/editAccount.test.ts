503497e27cc515ea2454ae2d694502d9e294edc6
Sho Sakuma <me@m1sk9.dev>
2024-02-08 10:52:01 +0900

    test: replace `RegisterService` to `InMemoryAccountRepository` (#121)

--- pkg/accounts/service/editAccount.test.ts
+++ pkg/accounts/service/editAccount.test.ts
@@ -1,33 +1,29 @@
-import { Result } from '@mikuroxina/mini-fn';
+import { Option, Result } from '@mikuroxina/mini-fn';
 import { describe, it, expect, afterEach } from 'vitest';
 
-import { type Clock, SnowflakeIDGenerator } from '../../id/mod.js';
+import type { ID } from '../../id/type.js';
 import { Argon2idPasswordEncoder } from '../../password/mod.js';
-import {
-  InMemoryAccountRepository,
-  InMemoryAccountVerifyTokenRepository,
-} from '../adaptor/repository/dummy.js';
-import { type AccountName, type AccountRole } from '../model/account.js';
+import { InMemoryAccountRepository } from '../adaptor/repository/dummy.js';
+import { Account, type AccountID } from '../model/account.js';
 import { EditAccountService } from './editAccount.js';
 import { EtagVerifyService } from './etagGenerateVerify.js';
-import { RegisterAccountService } from './register.js';
-import { DummySendNotificationService } from './sendNotification.js';
-import { TokenVerifyService } from './tokenVerify.js';
 
 const repository = new InMemoryAccountRepository();
-const verifyRepository = new InMemoryAccountVerifyTokenRepository();
-class DummyClock implements Clock {
-  Now(): bigint {
-    return BigInt(new Date('2023/9/10 00:00:00 UTC').getTime());
-  }
-}
-const registerService: RegisterAccountService = new RegisterAccountService({
-  repository,
-  idGenerator: new SnowflakeIDGenerator(1, new DummyClock()),
-  passwordEncoder: new Argon2idPasswordEncoder(),
-  sendNotification: new DummySendNotificationService(),
-  verifyTokenService: new TokenVerifyService(verifyRepository),
-});
+repository.create(
+  Account.new({
+    id: '1' as ID<AccountID>,
+    name: '@john@example.com',
+    mail: 'johndoe@example.com',
+    nickname: 'John Doe',
+    passphraseHash: 'hash',
+    bio: '',
+    role: 'normal',
+    frozen: 'normal',
+    silenced: 'normal',
+    status: 'notActivated',
+    createdAt: new Date(),
+  }),
+);
 const etagVerifyService = new EtagVerifyService();
 const editAccountService = new EditAccountService(
   repository,
@@ -35,119 +31,73 @@ const editAccountService = new EditAccountService(
   new Argon2idPasswordEncoder(),
 );
 
-const exampleInput = {
-  name: '@john_doe@example.com' as AccountName,
-  mail: 'johndoe@example.com',
-  nickname: 'John Doe',
-  passphrase: 'password',
-  bio: 'Hello, World!',
-  role: 'normal' as AccountRole,
-};
-
 describe('EditAccountService', () => {
   afterEach(() => repository.reset());
+
   it('should be success to update nickname', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    const etag = await etagVerifyService.generate(account);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
+
+    const etag = await etagVerifyService.generate(account[1]);
     const updateRes = await editAccountService.editNickname(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'new nickname',
     );
     expect(Result.isErr(updateRes)).toBe(false);
     expect(updateRes[1]).toBe(true);
-    expect(account.getNickname).toBe('new nickname');
+    expect(account[1].getNickname).toBe('new nickname');
   });
 
   it('should be fail to update nickname when nickname shorter more than 1', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    expect(Result.isErr(res)).toBe(false);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
-    const etag = await etagVerifyService.generate(account);
+    const etag = await etagVerifyService.generate(account[1]);
     const updateRes = await editAccountService.editNickname(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       '',
     );
     expect(Result.isErr(updateRes)).toBe(true);
   });
 
   it('should be fail to update nickname when nickname more than 256', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    expect(Result.isErr(res)).toBe(false);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
-    const etag = await etagVerifyService.generate(account);
+    const etag = await etagVerifyService.generate(account[1]);
     const updateRes = await editAccountService.editNickname(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'a'.repeat(257),
     );
     expect(Result.isErr(updateRes)).toBe(true);
   });
 
   it('should be success to update nickname when nickname 256', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    expect(Result.isErr(res)).toBe(false);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
-    const etag = await etagVerifyService.generate(account);
+    const etag = await etagVerifyService.generate(account[1]);
     const updateRes = await editAccountService.editNickname(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'a'.repeat(256),
     );
     expect(Result.isErr(updateRes)).toBe(false);
     expect(updateRes[1]).toBe(true);
-    expect(account.getNickname).toBe('a'.repeat(256));
+    expect(account[1].getNickname).toBe('a'.repeat(256));
   });
 
   it('should be success to update nickname when nickname 1', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    expect(Result.isErr(res)).toBe(false);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
-    const etag = await etagVerifyService.generate(account);
+    const etag = await etagVerifyService.generate(account[1]);
     const updateRes = await editAccountService.editNickname(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'a',
     );
     expect(Result.isErr(updateRes)).toBe(false);
@@ -155,18 +105,13 @@ describe('EditAccountService', () => {
   });
 
   it('should be fail to update nickname when etag not match', async () => {
-    await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
+    const etag = await etagVerifyService.generate(account[1]);
     const res = await editAccountService.editNickname(
-      'invalid_etag',
-      exampleInput.name,
+      etag,
+      '@john@example.com',
       'new nickname',
     );
     expect(Result.isErr(res)).toBe(true);
@@ -182,20 +127,13 @@ describe('EditAccountService', () => {
   });
 
   it('should be success to update passphrase', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    const etag = await etagVerifyService.generate(account);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
-    const updateRes = await editAccountService.editPassphrase(
+    const etag = await etagVerifyService.generate(account[1]);
+    const updateRes = await editAccountService.editNickname(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'new password',
     );
     expect(Result.isErr(updateRes)).toBe(false);
@@ -203,66 +141,39 @@ describe('EditAccountService', () => {
   });
 
   it('should be fail to update passphrase when passphrase shorter more than 8', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    expect(Result.isErr(res)).toBe(false);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
-    const etag = await etagVerifyService.generate(account);
-
-    const updateRes = await editAccountService.editPassphrase(
+    const etag = await etagVerifyService.generate(account[1]);
+    const updateRes = await editAccountService.editNickname(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'a'.repeat(7),
     );
     expect(Result.isErr(updateRes)).toBe(true);
   });
 
   it('should be fail to update passphrase when passphrase longer more than 512', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    expect(Result.isErr(res)).toBe(false);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
-    const etag = await etagVerifyService.generate(account);
-
-    const updateRes = await editAccountService.editPassphrase(
+    const etag = await etagVerifyService.generate(account[1]);
+    const updateRes = await editAccountService.editNickname(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'a'.repeat(513),
     );
     expect(Result.isErr(updateRes)).toBe(true);
   });
 
   it('should be success to update passphrase when passphrase 8', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    expect(Result.isErr(res)).toBe(false);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
-    const etag = await etagVerifyService.generate(account);
-
-    const updateRes = await editAccountService.editPassphrase(
+    const etag = await etagVerifyService.generate(account[1]);
+    const updateRes = await editAccountService.editNickname(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'a'.repeat(8),
     );
     expect(Result.isErr(updateRes)).toBe(false);
@@ -270,22 +181,13 @@ describe('EditAccountService', () => {
   });
 
   it('should be success to update passphrase when passphrase 512', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    expect(Result.isErr(res)).toBe(false);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
-    const etag = await etagVerifyService.generate(account);
-
-    const updateRes = await editAccountService.editPassphrase(
+    const etag = await etagVerifyService.generate(account[1]);
+    const updateRes = await editAccountService.editNickname(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'a'.repeat(512),
     );
     expect(Result.isErr(updateRes)).toBe(false);
@@ -293,18 +195,9 @@ describe('EditAccountService', () => {
   });
 
   it('should be fail to update passphrase when etag not match', async () => {
-    await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-
     const res = await editAccountService.editPassphrase(
       'invalid_etag',
-      exampleInput.name,
+      '@john@example.com',
       'new password',
     );
     expect(Result.isErr(res)).toBe(true);
@@ -320,20 +213,13 @@ describe('EditAccountService', () => {
   });
 
   it('should be success to update email', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    const etag = await etagVerifyService.generate(account);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
+    const etag = await etagVerifyService.generate(account[1]);
     const updateRes = await editAccountService.editEmail(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'pulsate@example.com',
     );
     expect(Result.isErr(updateRes)).toBe(false);
@@ -341,19 +227,12 @@ describe('EditAccountService', () => {
   });
 
   it('should be fail to update email when etag not match', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    Result.unwrap(res);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
     const updateRes = await editAccountService.editEmail(
       'invalid_etag',
-      exampleInput.name,
+      '@john@example.com',
       'pulsate@example.com',
     );
     expect(Result.isErr(updateRes)).toBe(true);
@@ -369,21 +248,13 @@ describe('EditAccountService', () => {
   });
 
   it('should be success to update email when email shortest', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    expect(Result.isErr(res)).toBe(false);
-    const etag = await etagVerifyService.generate(account);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
+    const etag = await etagVerifyService.generate(account[1]);
     const updateRes = await editAccountService.editEmail(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'a'.repeat(7),
     );
     expect(Result.isErr(updateRes)).toBe(false);
@@ -391,21 +262,13 @@ describe('EditAccountService', () => {
   });
 
   it('should be success to update email when email length 8', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    expect(Result.isErr(res)).toBe(false);
-    const etag = await etagVerifyService.generate(account);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
+    const etag = await etagVerifyService.generate(account[1]);
     const updateRes = await editAccountService.editEmail(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'a'.repeat(8),
     );
     expect(Result.isErr(updateRes)).toBe(false);
@@ -413,42 +276,26 @@ describe('EditAccountService', () => {
   });
 
   it('should be fail to update email when email too long', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    expect(Result.isErr(res)).toBe(false);
-    const etag = await etagVerifyService.generate(account);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
+    const etag = await etagVerifyService.generate(account[1]);
     const updateRes = await editAccountService.editEmail(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'a'.repeat(320),
     );
     expect(Result.isErr(updateRes)).toBe(true);
   });
 
   it('should be success to update email when email length 319', async () => {
-    const res = await registerService.handle(
-      exampleInput.name,
-      exampleInput.mail,
-      exampleInput.nickname,
-      exampleInput.passphrase,
-      exampleInput.bio,
-      exampleInput.role,
-    );
-    const account = Result.unwrap(res);
-    expect(Result.isErr(res)).toBe(false);
-    const etag = await etagVerifyService.generate(account);
+    const account = await repository.findByName('@john@example.com');
+    if (Option.isNone(account)) return;
 
+    const etag = await etagVerifyService.generate(account[1]);
     const updateRes = await editAccountService.editEmail(
       etag,
-      exampleInput.name,
+      '@john@example.com',
       'a'.repeat(319),
     );
     expect(Result.isErr(updateRes)).toBe(false);
