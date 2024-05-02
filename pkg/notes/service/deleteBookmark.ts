bab5682d8e7ba239796c9c1a898386b67b6344b3
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-05-02 15:06:13 +0900

    feat(notes/service/deleteBookmark): implement DeleteBookmarkService

---
+++ pkg/notes/service/deleteBookmark.ts
@@ -0,0 +1,17 @@
+import type { Result } from '@mikuroxina/mini-fn';
+
+import type { AccountID } from '../../accounts/model/account.js';
+import type { ID } from '../../id/type.js';
+import type { NoteID } from '../model/note.js';
+import type { BookmarkRepository } from '../model/repository.js';
+
+export class DeleteBookmarkService {
+  constructor(private readonly bookmarkRepository: BookmarkRepository) {}
+
+  async handle(
+    noteID: ID<NoteID>,
+    accountID: ID<AccountID>,
+  ): Promise<Result.Result<Error, void>> {
+    return await this.bookmarkRepository.deleteByID({ noteID, accountID });
+  }
+}
