26bc60b18ae2b42249f35bad8afc9a3fb1c0981d
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-05-06 22:51:39 +0900

    feat: 投稿ブックマークのControllerの実装 (#344)

---
+++ pkg/notes/adaptor/controller/bookmark.ts
@@ -0,0 +1,62 @@
+import { type Option, Result } from '@mikuroxina/mini-fn';
+
+import type { AccountID } from '../../../accounts/model/account.js';
+import type { ID } from '../../../id/type.js';
+import type { Bookmark } from '../../model/bookmark.js';
+import type { NoteID } from '../../model/note.js';
+import type { CreateBookmarkService } from '../../service/createBookmark.js';
+import type { DeleteBookmarkService } from '../../service/deleteBookmark.js';
+import type { FetchBookmarkService } from '../../service/fetchBookmark.js';
+
+export class BookmarkController {
+  constructor(
+    private readonly createBookmarkService: CreateBookmarkService,
+    private readonly fetchBookmarkService: FetchBookmarkService,
+    private readonly deleteBookmarkService: DeleteBookmarkService,
+  ) {}
+
+  async createBookmark(
+    noteID: string,
+    accountID: string,
+  ): Promise<Result.Result<Error, void>> {
+    const res = await this.createBookmarkService.handle(
+      noteID as ID<NoteID>,
+      accountID as ID<AccountID>,
+    );
+    return Result.map(() => undefined)(res);
+  }
+
+  async getBookmarkByID(
+    noteID: string,
+    accountID: string,
+  ): Promise<Option.Option<Bookmark>> {
+    const res = await this.fetchBookmarkService.fetchBookmarkByID(
+      noteID as ID<NoteID>,
+      accountID as ID<AccountID>,
+    );
+
+    return res;
+  }
+
+  async getBookmarkByAccountID(
+    accountID: string,
+  ): Promise<Option.Option<Bookmark[]>> {
+    const res = await this.fetchBookmarkService.fetchBookmarkByAccountID(
+      accountID as ID<AccountID>,
+    );
+
+    return res;
+  }
+
+  async deleteBookmark(
+    noteID: string,
+    accountID: string,
+  ): Promise<Result.Result<Error, void>> {
+    const res = await this.deleteBookmarkService.handle(
+      noteID as ID<NoteID>,
+      accountID as ID<AccountID>,
+    );
+
+    return Result.map(() => undefined)(res);
+  }
+}
