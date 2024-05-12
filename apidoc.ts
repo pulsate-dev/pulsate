083c6cba37ae80ce396e3aa7f3402cf5c32b1300
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-05-12 16:56:33 +0900

    feat: impl OAS API Document exporter

---
+++ apidoc.ts
@@ -0,0 +1,11 @@
+import { writeFileSync } from 'fs';
+import { testClient } from 'hono/testing';
+
+import { app } from './main.js';
+
+const client = testClient(app);
+const res = await client.doc.$get();
+const schema = await res.json();
+writeFileSync('./resources/schema.json', JSON.stringify(schema, null, 2));
+console.log('API Schema has been generated successfully.');
+process.exit(0);
