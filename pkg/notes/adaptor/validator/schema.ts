b73c52d22c561dc062189291d69bac2b6c51a11c
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-05-15 00:10:13 +0900

    feat(notes/bookmark): implement router, schema for bookmark (#364)

--- pkg/notes/adaptor/validator/schema.ts
+++ pkg/notes/adaptor/validator/schema.ts
@@ -155,3 +155,34 @@ export const RenoteResponseSchema = z.object({
     description: 'Note created date',
   }),
 });
+
+export const CreateBookmarkResponseSchema = z.object({
+  id: z.string().openapi({
+    example: '38477395',
+    description: 'Note ID',
+  }),
+  content: z.string().openapi({
+    example: 'hello world!',
+    description: 'Note content',
+  }),
+  visibility: z.string().openapi({
+    example: 'PUBLIC',
+    description: 'Note visibility',
+  }),
+  original_note_id: z.string().openapi({
+    example: '38477395',
+    description: 'Original note ID',
+  }),
+  contents_warning_comment: z.string().openapi({
+    example: 'This note contains sensitive content',
+    description: 'Contents warning comment',
+  }),
+  author_id: z.string().openapi({
+    example: '38477395',
+    description: 'Author account ID',
+  }),
+  created_at: z.string().openapi({
+    example: '2021-01-01T00:00:00Z',
+    description: 'Note created date',
+  }),
+});
