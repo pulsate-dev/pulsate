c0ae340d76e34d29a4f580e01ce331585efd7e6d
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-05-29 11:15:58 +0900

    feat(notes/router): recieve accountID as request json

--- pkg/notes/router.ts
+++ pkg/notes/router.ts
@@ -2,9 +2,11 @@ import { createRoute, z } from '@hono/zod-openapi';
 
 import { CommonErrorResponseSchema } from '../accounts/adaptor/validator/schema.js';
 import {
+  CreateBookmarkRequestSchema,
   CreateBookmarkResponseSchema,
   CreateNoteRequestSchema,
   CreateNoteResponseSchema,
+  DeleteBookmarkRequestSchema,
   GetNoteResponseSchema,
   RenoteRequestSchema,
   RenoteResponseSchema,
@@ -150,6 +152,13 @@ export const CreateBookmarkRoute = createRoute({
         example: '1',
       }),
     }),
+    body: {
+      content: {
+        'application/json': {
+          schema: CreateBookmarkRequestSchema,
+        },
+      },
+    },
   },
   responses: {
     200: {
@@ -182,6 +191,13 @@ export const DeleteBookmarkRoute = createRoute({
         example: '1',
       }),
     }),
+    body: {
+      content: {
+        'application/json': {
+          schema: DeleteBookmarkRequestSchema,
+        },
+      },
+    },
   },
   responses: {
     204: {
