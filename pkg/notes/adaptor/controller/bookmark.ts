8a2fd25b6f43fb992b1458966a9fe443e3b4cecf
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-05-29 11:18:32 +0900

    fix(notes/adaptor/controller/bookmark): createBookmark same type as response

--- pkg/notes/adaptor/controller/bookmark.ts
+++ pkg/notes/adaptor/controller/bookmark.ts
@@ -1,12 +1,14 @@
-import { type Option, Result } from '@mikuroxina/mini-fn';
+import type { z } from '@hono/zod-openapi';
+import { Result, type Option } from '@mikuroxina/mini-fn';
 
 import type { AccountID } from '../../../accounts/model/account.js';
 import type { ID } from '../../../id/type.js';
 import type { Bookmark } from '../../model/bookmark.js';
-import type { Note, NoteID } from '../../model/note.js';
+import type { NoteID } from '../../model/note.js';
 import type { CreateBookmarkService } from '../../service/createBookmark.js';
 import type { DeleteBookmarkService } from '../../service/deleteBookmark.js';
 import type { FetchBookmarkService } from '../../service/fetchBookmark.js';
+import type { CreateBookmarkResponseSchema } from '../validator/schema.js';
 
 export class BookmarkController {
   constructor(
@@ -18,13 +20,26 @@ export class BookmarkController {
   async createBookmark(
     noteID: string,
     accountID: string,
-  ): Promise<Result.Result<Error, Note>> {
+  ): Promise<
+    Result.Result<Error, z.infer<typeof CreateBookmarkResponseSchema>>
+  > {
     const res = await this.createBookmarkService.handle(
       noteID as ID<NoteID>,
       accountID as ID<AccountID>,
     );
 
-    return res;
+    if (Result.isErr(res)) {
+      return res;
+    }
+
+    return Result.ok({
+      id: res[1].getID(),
+      content: res[1].getContent(),
+      visibility: res[1].getVisibility(),
+      contents_warning_comment: res[1].getCwComment(),
+      author_id: res[1].getAuthorID(),
+      created_at: res[1].getCreatedAt().toUTCString(),
+    });
   }
 
   async getBookmarkByID(
