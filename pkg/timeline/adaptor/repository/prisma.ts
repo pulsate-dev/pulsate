28d5a28a2a4c4ac324d09312b9ddfb92eae5b372
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-06-10 16:41:13 +0900

    fix: use serializer at repository

--- pkg/timeline/adaptor/repository/prisma.ts
+++ pkg/timeline/adaptor/repository/prisma.ts
@@ -1,9 +1,13 @@
-import { Result } from '@mikuroxina/mini-fn';
-import type { PrismaClient } from '@prisma/client';
+import { Option, Result } from '@mikuroxina/mini-fn';
+import type { Prisma, PrismaClient } from '@prisma/client';
 
 import type { AccountID } from '../../../accounts/model/account.js';
 import type { ID } from '../../../id/type.js';
-import type { Note } from '../../../notes/model/note.js';
+import {
+  Note,
+  type NoteID,
+  type NoteVisibility,
+} from '../../../notes/model/note.js';
 import type {
   FetchAccountTimelineFilter,
   TimelineRepository,
@@ -11,6 +15,42 @@ import type {
 
 export class PrismaTimelineRepository implements TimelineRepository {
   constructor(private readonly prisma: PrismaClient) {}
+
+  private deserialize(
+    data: Prisma.PromiseReturnType<typeof this.prisma.note.findMany>,
+  ): Note[] {
+    return data.map((v) => {
+      const visibility = (): NoteVisibility => {
+        switch (v.visibility) {
+          case 0:
+            return 'PUBLIC';
+          case 1:
+            return 'HOME';
+          case 2:
+            return 'FOLLOWERS';
+          case 3:
+            return 'DIRECT';
+          default:
+            throw new Error('Invalid Visibility');
+        }
+      };
+      return Note.reconstruct({
+        id: v.id as ID<NoteID>,
+        content: v.text,
+        authorID: v.authorId as ID<AccountID>,
+        createdAt: v.createdAt,
+        deletedAt: !v.deletedAt ? Option.none() : Option.some(v.deletedAt),
+        contentsWarningComment: '',
+        originalNoteID: !v.renoteId
+          ? Option.some(v.renoteId as ID<NoteID>)
+          : Option.none(),
+        // ToDo: add SendTo field to db schema
+        sendTo: Option.none(),
+        updatedAt: Option.none(),
+        visibility: visibility() as NoteVisibility,
+      });
+    });
+  }
 
   async getAccountTimeline(
     accountId: ID<AccountID>,
@@ -29,6 +69,6 @@ export class PrismaTimelineRepository implements TimelineRepository {
       },
     });
     console.log(accountNotes);
-    return Result.err(new Error('Not implemented'));
+    return Result.ok(this.deserialize(accountNotes));
   }
 }
