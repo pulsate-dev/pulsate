0976ff15e4ba0b48fc2d912780185f10342fe3a5
m1sk9 <me@m1sk9.dev>
2024-07-21 17:25:55 +0900

    feat: Add `ReactionRepository` model

--- pkg/notes/model/repository.ts
+++ pkg/notes/model/repository.ts
@@ -4,6 +4,7 @@ import type { AccountID } from '../../accounts/model/account.js';
 import type { Medium, MediumID } from '../../drive/model/medium.js';
 import type { Bookmark } from './bookmark.js';
 import type { Note, NoteID } from './note.js';
+import type { Reaction } from './reaction.js';
 
 export interface NoteRepository {
   create(note: Note): Promise<Result.Result<Error, void>>;
@@ -25,6 +26,25 @@ export interface BookmarkRepository {
     accountID: AccountID;
   }): Promise<Option.Option<Bookmark>>;
   findByAccountID(id: AccountID): Promise<Option.Option<Bookmark[]>>;
+  deleteByID(id: {
+    noteID: NoteID;
+    accountID: AccountID;
+  }): Promise<Result.Result<Error, void>>;
+}
+
+export interface ReactionRepository {
+  create(
+    id: {
+      noteID: NoteID;
+      accountID: AccountID;
+    },
+    emoji: string,
+  ): Promise<Result.Result<Error, void>>;
+  findByID(id: {
+    noteID: NoteID;
+    accountID: AccountID;
+  }): Promise<Option.Option<Reaction>>;
+  findByAccountID(id: AccountID): Promise<Option.Option<Reaction[]>>;
   deleteByID(id: {
     noteID: NoteID;
     accountID: AccountID;
