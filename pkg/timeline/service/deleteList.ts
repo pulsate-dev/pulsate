c4dfa63d96d94e3e16d0cfd84525dc3f7a790b91
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-07-21 20:13:09 +0900

    feat(timeline/service): create `DeleteListService`

---
+++ pkg/timeline/service/deleteList.ts
@@ -0,0 +1,18 @@
+import { Result } from '@mikuroxina/mini-fn';
+import type { ID } from '../../id/type.js';
+import type { List } from '../model/list.js';
+import type { ListRepository } from '../model/repository.js';
+
+export class DeleteListService {
+  constructor(private readonly listRepository: ListRepository) {}
+
+  async handle(listId: ID<List>): Promise<Result.Result<Error, void>> {
+    const res = await this.listRepository.deleteById(listId);
+
+    if (Result.isErr(res)) {
+      return res;
+    }
+
+    return Result.ok(undefined);
+  }
+}
