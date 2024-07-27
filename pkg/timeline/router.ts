c6b677b9f8d73caf7309a5e9de6b2feae627fe6e
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-07-27 13:23:11 +0900

    fix: remove unnecessary routes

--- pkg/timeline/router.ts
+++ pkg/timeline/router.ts
@@ -62,57 +62,6 @@ export const GetAccountTimelineRoute = createRoute({
   },
 });
 
-export const PushNoteToTimelineRoute = createRoute({
-  method: 'post',
-  description: '',
-  tags: ['timeline'],
-  path: '/timeline/',
-  request: {
-    body: {
-      content: {
-        'application/json': {
-          schema: z.object({
-            id: z.string(),
-            authorId: z.string(),
-          }),
-        },
-      },
-    },
-  },
-  responses: {
-    204: {
-      description: 'OK',
-    },
-  },
-});
-
-export const DropNoteFromTimelineRoute = createRoute({
-  method: 'delete',
-  description: '',
-  tags: ['timeline'],
-  path: '/timeline/:id',
-  request: {
-    body: {
-      content: {
-        'application/json': {
-          schema: z.object({}),
-        },
-      },
-    },
-    params: z.object({
-      id: z.string().openapi('note authorID'),
-    }),
-  },
-  responses: {
-    204: {
-      description: 'OK',
-    },
-    500: {
-      description: 'Task failed',
-    },
-  },
-});
-
 export const CreateListRoute = createRoute({
   method: 'post',
   tags: ['timeline'],
