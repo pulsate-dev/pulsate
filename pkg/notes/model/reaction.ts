071cc2ae9eb271fb7fa8d66986f2ded7323c9143
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-08-03 13:28:48 +0900

    feat: Add `Reaction` model

---
+++ pkg/notes/model/reaction.ts
@@ -0,0 +1,35 @@
+import type { AccountID } from '../../accounts/model/account.js';
+import type { NoteID } from './note.js';
+
+export interface CreateReactionArgs {
+  accountID: AccountID;
+  noteID: NoteID;
+  body: string;
+}
+
+export class Reaction {
+  private constructor(arg: CreateReactionArgs) {
+    this.accountID = arg.accountID;
+    this.noteID = arg.noteID;
+    this.body = arg.body;
+  }
+
+  static new(arg: CreateReactionArgs) {
+    return new Reaction(arg);
+  }
+
+  private readonly accountID: AccountID;
+  getAccountID(): AccountID {
+    return this.accountID;
+  }
+
+  private readonly noteID: NoteID;
+  getNoteID(): NoteID {
+    return this.noteID;
+  }
+
+  private readonly body: string;
+  getBody(): string {
+    return this.body;
+  }
+}
