e089a5c25095294acfed9facc094f6e611020742
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-08-17 22:09:33 +0900

    feat: implememt ListRepository

--- pkg/timeline/adaptor/repository/prisma.ts
+++ pkg/timeline/adaptor/repository/prisma.ts
@@ -2,13 +2,16 @@ import { Option, Result } from '@mikuroxina/mini-fn';
 import type { Prisma, PrismaClient } from '@prisma/client';
 
 import type { AccountID } from '../../../accounts/model/account.js';
+import type { prismaClient } from '../../../adaptors/prisma.js';
 import {
   Note,
   type NoteID,
   type NoteVisibility,
 } from '../../../notes/model/note.js';
+import { List, type ListID } from '../../model/list.js';
 import type {
   FetchAccountTimelineFilter,
+  ListRepository,
   TimelineRepository,
 } from '../../model/repository.js';
 
@@ -17,7 +20,7 @@ export class PrismaTimelineRepository implements TimelineRepository {
   constructor(private readonly prisma: PrismaClient) {}
 
   private deserialize(
-    data: Prisma.PromiseReturnType<typeof this.prisma.note.findMany>,
+    data: Prisma.PromiseReturnType<typeof this.prisma.note.findMany & {}>,
   ): Note[] {
     return data.map((v) => {
       const visibility = (): NoteVisibility => {
@@ -109,5 +112,203 @@ export class PrismaTimelineRepository implements TimelineRepository {
       },
     });
     return Result.ok(this.deserialize(listNotes));
+  }
+}
+
+type DeserializeListArgs =
+  | (Prisma.PromiseReturnType<typeof prismaClient.list.findUnique> & {
+      listMember: {
+        memberId: string;
+      }[];
+    })
+  | null;
+
+export class PrismaListRepository implements ListRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  private deserialize(data: DeserializeListArgs): List {
+    if (!data) {
+      throw new Error('Invalid List Data');
+    }
+    return List.new({
+      id: data.id as ListID,
+      title: data.title,
+      ownerId: data.accountId as AccountID,
+      publicity: data.visibility === 1 ? 'PUBLIC' : 'PRIVATE',
+      createdAt: data.createdAt,
+      // ToDo: fill here
+      memberIds: data.listMember.map((v) => v.memberId as AccountID),
+    });
+  }
+
+  async create(list: List): Promise<Result.Result<Error, void>> {
+    try {
+      await this.prisma.list.create({
+        data: {
+          id: list.getId(),
+          title: list.getTitle(),
+          // NOTE: private: 0, public: 1
+          visibility: list.isPublic() ? 1 : 0,
+          account: {
+            connect: {
+              id: list.getOwnerId(),
+            },
+          },
+          createdAt: list.getCreatedAt(),
+        },
+      });
+    } catch (e) {
+      return Result.err(e as Error);
+    }
+    return Result.ok(undefined);
+  }
+
+  async fetchListsByOwnerId(
+    ownerId: AccountID,
+  ): Promise<Result.Result<Error, List[]>> {
+    try {
+      const res = await this.prisma.list.findMany({
+        where: {
+          accountId: ownerId,
+          deletedAt: undefined,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+        include: {
+          listMember: {
+            where: {
+              deletedAt: undefined,
+            },
+            select: {
+              memberId: true,
+            },
+          },
+        },
+      });
+      return Result.ok(res.map((v) => this.deserialize(v)));
+    } catch (e) {
+      return Result.err(e as Error);
+    }
+  }
+
+  async fetchList(listId: ListID): Promise<Result.Result<Error, List>> {
+    try {
+      const res = await this.prisma.list.findUnique({
+        where: {
+          id: listId,
+          deletedAt: undefined,
+        },
+        include: {
+          listMember: {
+            where: {
+              deletedAt: undefined,
+            },
+            select: {
+              memberId: true,
+            },
+          },
+        },
+      });
+      return Result.ok(this.deserialize(res));
+    } catch (e) {
+      return Result.err(e as Error);
+    }
+  }
+
+  async fetchListMembers(
+    listId: ListID,
+  ): Promise<Result.Result<Error, AccountID[]>> {
+    try {
+      const res = await this.prisma.listMember.findMany({
+        where: {
+          listId: listId,
+          deletedAt: undefined,
+        },
+      });
+
+      if (!res) {
+        return Result.err(new Error('List member not found'));
+      }
+      return Result.ok(res.map((v) => v.memberId as AccountID));
+    } catch (e) {
+      return Result.err(e as Error);
+    }
+  }
+
+  async fetchListsByMemberAccountID(
+    accountID: AccountID,
+  ): Promise<Result.Result<Error, List[]>> {
+    try {
+      const res = await this.prisma.list.findMany({
+        where: {
+          deletedAt: undefined,
+          listMember: {
+            every: {
+              memberId: accountID,
+            },
+          },
+        },
+        include: {
+          listMember: {
+            where: {
+              deletedAt: undefined,
+            },
+            select: {
+              memberId: true,
+            },
+          },
+        },
+      });
+
+      return Result.ok(res.map((v) => this.deserialize(v)));
+    } catch (e) {
+      return Result.err(e as Error);
+    }
+  }
+
+  async edit(list: List): Promise<Result.Result<Error, void>> {
+    try {
+      await this.prisma.list.update({
+        where: {
+          id: list.getId(),
+        },
+        data: {
+          title: list.getTitle(),
+          visibility: list.isPublic() ? 1 : 0,
+        },
+      });
+
+      return Result.ok(undefined);
+    } catch (e) {
+      return Result.err(e as Error);
+    }
+  }
+
+  async deleteById(listId: ListID): Promise<Result.Result<Error, void>> {
+    try {
+      // NOTE: delete list
+      await this.prisma.list.updateMany({
+        where: {
+          id: listId,
+        },
+        data: {
+          deletedAt: new Date(),
+        },
+      });
+      // NOTE: then delete list members
+      await this.prisma.listMember.updateMany({
+        where: {
+          listId: listId,
+        },
+        data: {
+          deletedAt: new Date(),
+        },
+      });
+
+      return Result.ok(undefined);
+    } catch (e) {
+      return Result.err(e as Error);
+    }
   }
 }
