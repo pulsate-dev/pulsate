bb5eaf359df1c8affe18e6c7cbc92f22c6790cf0
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-10-06 18:28:14 +0900

    chore: remove IIFE

--- pkg/accounts/adaptor/captcha/turnstile.ts
+++ pkg/accounts/adaptor/captcha/turnstile.ts
@@ -23,10 +23,10 @@ export class TurnstileCaptchaValidator implements Captcha {
     );
     const response = await res.json();
     if (!response.success) {
-      const errorResponse = (({ success, ...rest }) => rest)(response);
+      const { success, ...rest } = response;
       return Option.some(
         new AccountCaptchaTokenInvalidError('failed to verify captcha token', {
-          cause: errorResponse,
+          cause: rest,
         }),
       );
     }
