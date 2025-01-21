9ebc90afcc2555f0a0f1b3d22b7b3bd51963d921
Tatsuto YAMAMOTO <me@laminne33569.net>
2025-01-21 17:26:22 +0900

    chore: impl AuthenticationTokenService.renewAuthToken

--- pkg/accounts/service/authenticationTokenService.test.ts
+++ pkg/accounts/service/authenticationTokenService.test.ts
@@ -1,8 +1,15 @@
-import { Option } from '@mikuroxina/mini-fn';
+import { Option, Result } from '@mikuroxina/mini-fn';
+import * as jose from 'jose';
 import { describe, expect, it } from 'vitest';
 
 import { MockClock } from '../../id/mod.js';
 import { AuthenticationTokenService } from './authenticationTokenService.js';
+
+class Clock {
+  now() {
+    return BigInt(Date.now());
+  }
+}
 
 describe('AuthenticationTokenService', () => {
   it('verify JWT Token', async () => {
@@ -22,8 +29,45 @@ describe('AuthenticationTokenService', () => {
       new MockClock(new Date('2022-01-01T00:00:00Z')),
     );
     const expired = await service.generate('', '');
+    expect(Option.isSome(expired)).toBe(true);
+
+    expect(await service.verify(Option.unwrap(expired))).toBe(false);
+  });
+
+  it('renew: if token is valid, it should return a new token', async () => {
+    // NOTE: mockClock returns only *static* time, this test using a normal clock
+    const service = await AuthenticationTokenService.new(new Clock());
+    const token = await service.generate('314', '628');
+    expect(Option.isSome(token)).toBe(true);
+
+    const { refreshToken: oldRefreshToken, ...oldPayload } = jose.decodeJwt(
+      Option.unwrap(token),
+    );
+
+    await (async (ms) => new Promise((resolve) => setTimeout(resolve, ms)))(
+      1000,
+    );
+
+    const renewed = await service.renewAuthToken(Option.unwrap(token));
+    const { refreshToken: newRefreshToken, ...newPayload } = jose.decodeJwt(
+      Result.unwrap(renewed),
+    );
+
+    // renewed token must be valid token
+    expect(await service.verify(Result.unwrap(renewed))).toBe(true);
+
+    expect(oldPayload).not.toStrictEqual(newPayload);
+    expect(oldRefreshToken).toStrictEqual(newRefreshToken);
+  });
+
+  it('renew: if authToken is expired, it should return error', async () => {
+    const service = await AuthenticationTokenService.new(
+      new MockClock(new Date('2022-01-01T00:00:00Z')),
+    );
+    const expired = await service.generate('314', '628');
     if (Option.isNone(expired)) return;
 
-    expect(await service.verify(expired[1])).toBe(false);
+    const result = await service.renewAuthToken(Option.unwrap(expired));
+    expect(Result.isErr(result)).toBe(true);
   });
 });
