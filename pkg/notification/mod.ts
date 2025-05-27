86c0561324c5a034ed810d6b569e30742990f118
Tatsuto YAMAMOTO <me@laminne33569.net>
2025-05-27 22:43:15 +0900

    feat: 通知を既読にする `POST /v0/notification/:id/read`を実装 (#1073)

---
+++ pkg/notification/mod.ts
@@ -0,0 +1,71 @@
+import { OpenAPIHono } from '@hono/zod-openapi';
+import { Cat, Ether, Option, Result } from '@mikuroxina/mini-fn';
+import {
+  type AuthMiddlewareVariable,
+  AuthenticateMiddlewareService,
+} from '../adaptors/authenticateMiddleware.js';
+import { isProduction } from '../adaptors/env.js';
+import { prismaClient } from '../adaptors/prisma.js';
+import { clockSymbol } from '../id/mod.js';
+import { NotificationController } from './adaptor/controller/notification.js';
+import { inMemoryNotificationRepo } from './adaptor/repository/dummy/notification.js';
+import { prismaNotificationRepo } from './adaptor/repository/prisma/notification.js';
+import { PostMakeAsReadNotificationRoute } from './routes.js';
+import { markAsReadNotification } from './service/markAsRead.js';
+
+class Clock {
+  now() {
+    return BigInt(Date.now());
+  }
+}
+const clock = Ether.newEther(clockSymbol, () => new Clock());
+
+const notificationRepository = isProduction
+  ? prismaNotificationRepo(prismaClient)
+  : inMemoryNotificationRepo();
+
+const controller = new NotificationController({
+  markAsReadService: Ether.runEther(
+    Cat.cat(markAsReadNotification)
+      .feed(Ether.compose(notificationRepository))
+      .feed(Ether.compose(clock)).value,
+  ),
+});
+
+export const notification = new OpenAPIHono<{
+  Variables: AuthMiddlewareVariable;
+}>().doc31('/notification/doc.json', {
+  openapi: '3.1.0',
+  info: {
+    title: 'Notification API',
+    version: '0.1.0',
+  },
+});
+const AuthMiddleware = new AuthenticateMiddlewareService();
+
+notification.openAPIRegistry.registerComponent('securitySchemes', 'Bearer', {
+  type: 'http',
+  scheme: 'bearer',
+});
+
+notification[PostMakeAsReadNotificationRoute.method](
+  PostMakeAsReadNotificationRoute.path,
+  AuthMiddleware.handle({ forceAuthorized: true }),
+);
+notification.openapi(PostMakeAsReadNotificationRoute, async (c) => {
+  const actorID = Option.unwrap(c.get('accountID'));
+  const { id } = c.req.valid('param');
+
+  const res = await controller.markAsRead(id, actorID);
+  if (Result.isErr(res)) {
+    const error = Result.unwrapErr(res);
+
+    if (error.message === 'Not allowed') {
+      return c.json({ error: 'NO_PERMISSION' as const }, 403);
+    }
+
+    return c.json({ error: 'INTERNAL_ERROR' as const }, 500);
+  }
+
+  return new Response(undefined, { status: 204 });
+});
---
+++ pkg/notification/service/markAsRead.ts
@@ -0,0 +1,63 @@
+import { Ether, Result } from '@mikuroxina/mini-fn';
+import type { AccountID } from '../../accounts/model/account.js';
+import { type Clock, clockSymbol } from '../../id/mod.js';
+import type {
+  Notification,
+  NotificationID,
+} from '../model/notificationBase.js';
+import {
+  type NotificationRepository,
+  notificationRepoSymbol,
+} from '../model/repository/notification.js';
+
+export class MarkAsReadNotificationService {
+  constructor(
+    private readonly notificationRepository: NotificationRepository,
+    private readonly clock: Clock,
+  ) {}
+
+  async handle(
+    notificationID: NotificationID,
+    actorID: AccountID,
+  ): Promise<Result.Result<Error, Notification>> {
+    const notificationRes =
+      await this.notificationRepository.findByID(notificationID);
+    if (Result.isErr(notificationRes)) {
+      return notificationRes;
+    }
+
+    const notification = Result.unwrap(notificationRes);
+
+    if (!this.isAllowed(notification, actorID)) {
+      return Result.err(new Error('Not allowed'));
+    }
+
+    if (notification.getIsRead()) {
+      return Result.err(new Error('Notification already read'));
+    }
+
+    notification.setRead(new Date(Number(this.clock.now())));
+
+    const res = await this.notificationRepository.updateReadAt(notification);
+    if (Result.isErr(res)) {
+      return res;
+    }
+
+    return Result.ok(notification);
+  }
+
+  private isAllowed(notification: Notification, accountID: AccountID): boolean {
+    return notification.getRecipientID() === accountID;
+  }
+}
+export const markAsReadNotificationSymbol =
+  Ether.newEtherSymbol<MarkAsReadNotificationService>();
+export const markAsReadNotification = Ether.newEther(
+  markAsReadNotificationSymbol,
+  ({ notificationRepository, clock }) =>
+    new MarkAsReadNotificationService(notificationRepository, clock),
+  {
+    notificationRepository: notificationRepoSymbol,
+    clock: clockSymbol,
+  },
+);
