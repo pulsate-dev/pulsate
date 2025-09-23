8b4f0e7550963b94a1aecd366738006a9ec600ff
Tatsuto YAMAMOTO <me@laminne33569.net>
2025-09-23 21:40:39 +0900

    chore: fix broken test

--- pkg/accounts/service/register.test.ts
+++ pkg/accounts/service/register.test.ts
@@ -2,7 +2,7 @@ import { Result } from '@mikuroxina/mini-fn';
 import { describe, expect, it } from 'vitest';
 
 import { MockClock, SnowflakeIDGenerator } from '../../id/mod.js';
-import { DummySendNotificationService } from '../../notification/service/sendEmailNotification.js';
+import { notificationModule } from '../../intermodule/notification.js';
 import { Argon2idPasswordEncoder } from '../../password/mod.js';
 import { InMemoryAccountRepository } from '../adaptor/repository/dummy/account.js';
 import { InMemoryAccountVerifyTokenRepository } from '../adaptor/repository/dummy/verifyToken.js';
@@ -18,7 +18,7 @@ const registerService: RegisterService = new RegisterService({
   repository,
   idGenerator: new SnowflakeIDGenerator(1, mockClock),
   passwordEncoder: new Argon2idPasswordEncoder(),
-  sendNotification: new DummySendNotificationService(),
+  notificationModule: notificationModule,
   verifyAccountTokenService: new VerifyAccountTokenService(
     verifyRepository,
     repository,
