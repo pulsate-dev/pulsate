b9537c5fd80a229fcf2e510e9c12d2f69f810fd9
Tatsuto YAMAMOTO <me@laminne33569.net>
2025-11-27 13:42:47 +0900

    chore: 初期化用の変数チェックをしない

--- pkg/adaptors/prisma.ts
+++ pkg/adaptors/prisma.ts
@@ -1,10 +1,6 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/client/client.js';
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set');
-}
-
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
