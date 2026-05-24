0993d9881ff81880e707455680ddeff891386d0c
Tatsuto YAMAMOTO <me@laminne33569.net>
2026-05-24 14:23:11 +0900

    chore: 正しいunicode範囲指定に変える

--- pkg/notes/adaptor/validator/schema.ts
+++ pkg/notes/adaptor/validator/schema.ts
@@ -47,7 +47,7 @@ export const noteAttachmentSchema = z.object({
 const zodEmojiSchema = z
   .string()
   .regex(
-    /[✀-➿]|[-]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[‑-⛿]|\uD83E[\uDD10-\uDDFF]|<:(\w+):(\d+)>/,
+    /[\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF]/,
   );
 
 export const reactionSchema = z.object({
