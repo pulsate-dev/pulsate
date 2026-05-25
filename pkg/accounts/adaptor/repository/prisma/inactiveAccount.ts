73f7b97bd9730d7aefbb01292a357558dcf1324d
Tatsuto YAMAMOTO <me@laminne33569.net>
2026-05-26 00:20:47 +0900

    feat: PrismaInactiveAccountを実装

---
+++ pkg/accounts/adaptor/repository/prisma/inactiveAccount.ts
@@ -0,0 +1,82 @@
+import { Ether, Option, Result } from '@mikuroxina/mini-fn';
+import type { PrismaClient } from '../../../../adaptors/prisma/client.js';
+import type { prismaClient } from '../../../../adaptors/prisma.js';
+import type { AccountID, AccountName } from '../../../model/account.js';
+import { InactiveAccount } from '../../../model/inactiveAccount.js';
+import {
+  type InactiveAccountRepository,
+  inactiveAccountRepoSymbol,
+} from '../../../model/repository.js';
+import { parsePrismaError } from './prisma.js';
+
+type InactiveAccountPrismaArgs = Awaited<
+  ReturnType<typeof prismaClient.inactiveAccount.findUnique>
+>;
+
+export class PrismaInactiveAccountRepository
+  implements InactiveAccountRepository
+{
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async create(account: InactiveAccount): Promise<Result.Result<Error, void>> {
+    try {
+      await this.prisma.inactiveAccount.create({
+        data: {
+          id: account.getID(),
+          name: account.getName(),
+          mail: account.getMail(),
+        },
+      });
+    } catch (e) {
+      return Result.err(parsePrismaError(e));
+    }
+    return Result.ok(undefined);
+  }
+
+  async findByName(name: string): Promise<Option.Option<InactiveAccount>> {
+    const res = await this.prisma.inactiveAccount.findUnique({
+      where: { name },
+    });
+    if (!res) {
+      return Option.none();
+    }
+    return Option.some(this.fromPrismaArgs(res));
+  }
+
+  async findByMail(mail: string): Promise<Option.Option<InactiveAccount>> {
+    const res = await this.prisma.inactiveAccount.findUnique({
+      where: { mail },
+    });
+    if (!res) {
+      return Option.none();
+    }
+    return Option.some(this.fromPrismaArgs(res));
+  }
+
+  async delete(id: AccountID): Promise<Result.Result<Error, void>> {
+    try {
+      await this.prisma.inactiveAccount.delete({
+        where: { id },
+      });
+    } catch (e) {
+      return Result.err(parsePrismaError(e));
+    }
+    return Result.ok(undefined);
+  }
+
+  private fromPrismaArgs(args: InactiveAccountPrismaArgs): InactiveAccount {
+    if (args === null) {
+      throw new Error('failed to serialize');
+    }
+    return InactiveAccount.reconstruct({
+      id: args.id as AccountID,
+      name: args.name as AccountName,
+      mail: args.mail,
+    });
+  }
+}
+export const prismaInactiveAccountRepo = (client: PrismaClient) =>
+  Ether.newEther(
+    inactiveAccountRepoSymbol,
+    () => new PrismaInactiveAccountRepository(client),
+  );
